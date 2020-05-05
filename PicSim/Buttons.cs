e5a692e99e862351cbe96ee638eb2f196d2348fa
Summa1337 <Marcel.sommer1994@gmx.de>
2020-05-05 10:36:36 +0200

    buttons.cs Variables.cs

---
+++ PicSim/Buttons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace PicSim
+{
+
+
+    public class Buttons
+    {
+
+		/*public void GoButton()
+		{
+			
+			for (int i = 0; i <= TransferPCLength();i++)
+			{
+				//cmd.Switchcase(i);
+			}
+		}
+		
+        public void StepButton()
+        {
+
+            
+            //cmd.Switchcase(programcounter);
+        }
+		*/
+    }
+}
+
+//sichtbarkeit der variabeln in c# -> programcounter
+//klassen ineinander einbinden -> methoden anderer klassen aufrufen
---
+++ PicSim/Variables.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PicSim
+{
+    public static class Globals
+    {
+        public static int programcounter; // Modifiable
+        public static int[] programmemory;
+        public static int w;
+        public static int Getprogramcounter
+        {
+            get
+            {
+                return programcounter;
+            }
+            set
+            {
+                programcounter = value;
+            }
+
+        }
+    }
+
+}
