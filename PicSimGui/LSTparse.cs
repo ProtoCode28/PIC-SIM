e28e964904657f0b71fdf9115bb892b2df683e47
ProtoCode28 <maier.maximilian2@student.dhbw-karlsruhe.de>
2020-05-29 09:41:28 +0200

    added new projectfiles

---
+++ PicSimGui/LSTparse.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+using System.Text.RegularExpressions;
+
+namespace PicSim
+{
+
+    public class LSTParse
+    {
+
+        public string path;
+
+        public Regex rx;
+
+
+        public LSTParse(string path)
+        {
+            this.path = path;
+            Globals.programmemory = new int[1024];
+
+            rx = new Regex(@"\b\d(\d|\w){3} (\d|\w){4}\b");
+        }
+
+        public void Einlesen()
+        {
+            string line;
+            MatchCollection matches;
+            System.IO.StreamReader file = new System.IO.StreamReader(@path);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                matches = rx.Matches(line);
+                foreach (Match match in matches)
+                {
+                    string result = match.Value;
+                    //System.Console.WriteLine(result);
+
+                    Globals.programmemory[Globals.programcounter = Umwandelnprogramcounter(result)] = Umwandelnhex(result);
+
+
+                }
+            }
+            file.Close();
+        }
+
+        public int Umwandelnhex(string res) //gleiche funktion wie Umwandelnprogramcounter aber andere Regex
+        {
+            Regex rx = new Regex(@"\b (\d|\w){4}\b");
+            Match match = rx.Match(res);
+            string var = match.Value;
+            int result = int.Parse(var, System.Globalization.NumberStyles.HexNumber);
+            return result;
+        }
+
+
+
+        public int Umwandelnprogramcounter(string res) //gleiche funktion wie Umwandelnhex aber andere Regex
+        {
+            Regex rx = new Regex(@"\b(\d|\w){4} \b");
+            Match match = rx.Match(res);
+            string var = match.Value;
+            int result = int.Parse(var, System.Globalization.NumberStyles.HexNumber);
+            return result;
+        }
+
+        public void Ausgeben()
+        {
+            for (int i = 0; i < Globals.programcounter; i++)
+            {
+                Console.WriteLine(Globals.programmemory[i]);
+                Console.WriteLine(Globals.programcounter);
+            }
+        }
+
+        public int Transferprogrammemory(int i)
+        {
+            return Globals.programmemory[i];
+        }
+
+        //public static void Main(string[] args, string path)
+        //{
+        //    LSTParse T1 = new LSTParse(path); //Pfad Marcel @"D:\TPicSim6.LST" //Pfad Max @"C:\vm\TPicSim6.LST"
+        //    T1.Einlesen();
+        //    T1.Ausgeben();
+        //    Register R1 = new Register();
+        //    R1.PowerReset();
+        //    //R1.Ausgeben();
+        //    //Commands cmd = new Commands();
+        //    //cmd.Switchcase(Globals.programcounter); wird von buttons aufgerufen
+
+        //    Buttons but = new Buttons();
+        //  /*  but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton();
+        //    but.StepButton(); */
+
+        //    but.GoButton();
+
+        //}
+    }
+}
---
+++ PicSimGui/Register.cs
@@ -0,0 +1,100 @@
+﻿
+namespace PicSim
+{
+    public class Register
+    {
+
+
+        public Register()
+        {
+            Globals.bank0 = new int[128];//Globale Variable
+            Globals.bank1 = new int[128];//
+        }
+        public void PowerReset()
+        {
+            Globals.bank0[2] = int.Parse("0000", System.Globalization.NumberStyles.HexNumber);     //PCL
+            Globals.bank0[3] = int.Parse("0018", System.Globalization.NumberStyles.HexNumber);     //Statusregister Bit0 = c, Bit1 = DC, Bit2 = z; bit3 = PD; bit4 = TO
+            Globals.bank0[10] = int.Parse("0000", System.Globalization.NumberStyles.HexNumber);    //PCLATH
+            Globals.bank0[11] = int.Parse("0000", System.Globalization.NumberStyles.HexNumber);    //INTCON
+
+            Globals.bank1[1] = int.Parse("00FF", System.Globalization.NumberStyles.HexNumber);     //OPTION
+            Globals.bank1[2] = int.Parse("0000", System.Globalization.NumberStyles.HexNumber);     //PCL
+            Globals.bank1[3] = int.Parse("0018", System.Globalization.NumberStyles.HexNumber);     //Statusregister
+            Globals.bank1[5] = int.Parse("00FF", System.Globalization.NumberStyles.HexNumber);     //TRISA
+            Globals.bank1[6] = int.Parse("00FF", System.Globalization.NumberStyles.HexNumber);     //TRISB
+            Globals.bank1[8] = int.Parse("0000", System.Globalization.NumberStyles.HexNumber);     //nicht benannt
+            Globals.bank1[10] = int.Parse("0000", System.Globalization.NumberStyles.HexNumber);    //PCLATH
+            Globals.bank1[11] = int.Parse("0000", System.Globalization.NumberStyles.HexNumber);    //INTCON
+            Globals.programcounter = 0;
+
+        }
+
+        public static void Reset()
+        {
+            setByte(2, false, "cccccccc");
+            setByte(3, false, "cccxxxxx");
+            setByte(10, false, "cccccccc");
+            setByte(11, false, "cccccccx");
+
+            setByte(1, true, "ssssssss");
+            setByte(2, true, "cccccccc");
+            setByte(3, true, "cccxxxxx");
+            setByte(5, true, "cccsssss");
+            setByte(6, true, "ssssssss");
+            setByte(8, true, "ccccxccc");
+            setByte(10, true, "cccccccc");
+            setByte(11, true, "cccccccx");
+            Globals.programcounter = 0;
+
+        }
+
+        public static void setByte(int adresse, bool bank, string mask)
+        {
+            if(mask.Length != 8)
+            {
+                System.Console.WriteLine("ERROR: length of mask not correct");
+                return;
+            }
+            for (int i = 7; i > 0; i--)
+            {
+                if (mask[i] == 's')
+                {
+                    if (bank == false)
+                    {
+                        Globals.bank0[adresse] |= (1 << (7-i));
+                    }
+                    else
+                    {
+                        Globals.bank1[adresse] |= (1 << (7-i));
+                    }
+                }
+                if (mask[i] == 'c')
+                {
+                    if (bank == false)
+                    {
+                        Globals.bank0[adresse] &= ~(1 << i);
+                    }
+                    else
+                    {
+                        Globals.bank1[adresse] &= ~(1 << i);
+                    }
+                }
+            }
+        }
+
+
+
+public static void Ausgeben()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                System.Console.WriteLine("Bank 0:" + "Stelle: " + i + " " + Globals.bank0[i]);
+                System.Console.WriteLine("Bank 1:" + "Stelle: " + i + " " + Globals.bank1[i]);
+
+            }
+        }
+    }
+}
+//indirekte adressierung
+//pcl als zieladresse müssen wir überlegen ob wir uns eine methode schreiben die die einzelnen mehtoden aufrufen
+//für UI hilfsmethode die UI regelmäßig refreshed nach jedem Step/execution
