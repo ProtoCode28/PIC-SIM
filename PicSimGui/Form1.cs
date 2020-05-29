3a4bbfcd7c85d275137e981f2aa0f1053e8cda02
ProtoCode28 <maier.maximilian2@student.dhbw-karlsruhe.de>
2020-05-29 09:39:40 +0200

    united two projects into one

--- PicSimGui/Form1.cs
+++ PicSimGui/Form1.cs
@@ -16,5 +16,38 @@ namespace PicSim
         {
             InitializeComponent();
         }
+
+        private void ladenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            Register.Reset();
+            //Ausgabe.Text = "";
+            
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    //LSTparse.Main(filePath);
+
+                    //foreach (String value in parser.matches)
+                    //{
+                    //    Ausgabe.Text += value + "\r\n";
+                    //    umwandlung.EingabeAuseinanderschneiden(value);
+
+                    //}
+
+                }
+            }
+        }
     }
 }
